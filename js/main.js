7b5c54d96296f4d4347c39ed42843bd826a90592
victor <vgarzom@gmail.com>
2019-03-28 13:30:19 -0500

    updates department codes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 const width = 600;
 const margin = { top: 50, left: 100, right: 50, bottom: 0 };
 const svg = d3.select("#chart-container").append("svg");
-let g = svg.append("g");
-let gText = svg.append("g");
-let gAxis = svg.append("g");
+const g = svg.append("g");
+const gText = svg.append("g");
+const gAxis = svg.append("g");
+const minLabel = margin.left + 150;
+const transitionDuration = 500;
+let height;
+let x; // X scale
+const xScaleRange = [margin.left, width - margin.right];
 
 let dataAll = [];
 let dataG = [];
 
-let height;
 let value_key = "total_casos";
 let total_key = "total";
-const minLabel = margin.left + 150;
 let currentSlide = 0;
-let transitionDuration = 500;
 let drawingByGender = false;
 let drawStacked = false;
 let drawCentralLine = false;
@@ -24,10 +26,6 @@ let updateBarChart = (data, sortedNames) => {
   height = (drawingByGender ? data.length / 2 : data.length) * 22 + margin.top + margin.bottom;
 
   //Defines the scales
-  const x = d3.scaleLinear()
-    .domain([0, d3.max(data, d => { return drawStacked ? d[total_key] : d[value_key] })])
-    .range([margin.left, width - margin.right]);
-
   const y = d3.scaleBand()
     .domain(sortedNames)
     .range([margin.top, height - margin.bottom])
@@ -85,39 +83,39 @@ let updateBarChart = (data, sortedNames) => {
   }
 
 
-  
-    //Draw text
-    /*
-    gText.selectAll("text").data(data).exit().remove();
-    gText.style("font", "13px sans-serif")
-      .selectAll("text")
-      .data(data)
-      .enter().append("text")
-      .attr("x", d => {
-        const v = x(d[value_key]);
-        return v + (v < minLabel ? 4 : -4)
-      })
-      .attr("class", "label_test")
-      .attr("text-anchor", d => (x(d[value_key]) < minLabel ? "start" : "end"))
-      .attr("fill", d => (x(d[value_key]) < minLabel ? "currentColor" : "white"))
-      .attr("fill-opacity", d => (x(d[value_key]) < minLabel ? .7 : 1))
-      .attr("y", d => y(d.nombre) + y.bandwidth() / 2)
-      .attr("dy", "0.35em")
-      .text(d => d[value_key]);
-  
-    gText.selectAll(".label_test")
-      .transition()
-      .duration(transitionDuration)
-      .delay(delay)
-      .attr("x", d => {
-        const v = x(d[value_key]);
-        return v + (v < minLabel ? 4 : -4)
-      })
-      .attr("text-anchor", d => (x(d[value_key]) < minLabel ? "start" : "end"))
-      .attr("fill", d => (x(d[value_key]) < minLabel ? "currentColor" : "white"))
-      .attr("fill-opacity", d => (x(d[value_key]) < minLabel ? .7 : 1))
-      .text(d => d[value_key]);
-  */
+
+  //Draw text
+  /*
+  gText.selectAll("text").data(data).exit().remove();
+  gText.style("font", "13px sans-serif")
+    .selectAll("text")
+    .data(data)
+    .enter().append("text")
+    .attr("x", d => {
+      const v = x(d[value_key]);
+      return v + (v < minLabel ? 4 : -4)
+    })
+    .attr("class", "label_test")
+    .attr("text-anchor", d => (x(d[value_key]) < minLabel ? "start" : "end"))
+    .attr("fill", d => (x(d[value_key]) < minLabel ? "currentColor" : "white"))
+    .attr("fill-opacity", d => (x(d[value_key]) < minLabel ? .7 : 1))
+    .attr("y", d => y(d.nombre) + y.bandwidth() / 2)
+    .attr("dy", "0.35em")
+    .text(d => d[value_key]);
+ 
+  gText.selectAll(".label_test")
+    .transition()
+    .duration(transitionDuration)
+    .delay(delay)
+    .attr("x", d => {
+      const v = x(d[value_key]);
+      return v + (v < minLabel ? 4 : -4)
+    })
+    .attr("text-anchor", d => (x(d[value_key]) < minLabel ? "start" : "end"))
+    .attr("fill", d => (x(d[value_key]) < minLabel ? "currentColor" : "white"))
+    .attr("fill-opacity", d => (x(d[value_key]) < minLabel ? .7 : 1))
+    .text(d => d[value_key]);
+*/
   // Draw Axes
   let axisTransition = gAxis.transition().duration(transitionDuration);
   axisTransition.select(".y.axis")
@@ -151,6 +149,11 @@ let drawBySlide = () => {
       value_key = "total_casos";
       var sortedData = dataAll.sort((a, b) => d3.ascending(a.nombre, b.nombre))
         .map(d => d.nombre);
+
+      x = d3.scaleLinear()
+        .domain([0, d3.max(dataAll, d => d.total_casos)])
+        .range(xScaleRange);
+
       updateBarChart(dataAll, sortedData);
       break;
     case 1:
@@ -160,8 +163,12 @@ let drawBySlide = () => {
       value_key = "total_casos";
       var sortedData = dataAll.sort((a, b) => d3.descending(a.total_casos, b.total_casos))
         .map(d => d.nombre);
+      x = d3.scaleLinear()
+        .domain([0, d3.max(dataAll, d => d.total_casos)])
+        .range(xScaleRange);
       updateBarChart(dataAll, sortedData);
       break;
+
     case 2:
       drawingByGender = false;
       drawStacked = false;
@@ -169,8 +176,12 @@ let drawBySlide = () => {
       value_key = "total_porciento";
       var sortedData = dataAll.sort((a, b) => d3.descending(a.total_casos, b.total_casos))
         .map(d => d.nombre);
+      x = d3.scaleLinear()
+        .domain([0, d3.max(dataAll, d => d.total_porciento)])
+        .range(xScaleRange);
       updateBarChart(dataAll, sortedData);
       break;
+
     case 3:
       drawingByGender = false;
       drawStacked = false;
@@ -178,6 +189,9 @@ let drawBySlide = () => {
       value_key = "total_porciento";
       var sortedData = dataAll.sort((a, b) => d3.descending(a.total_porciento, b.total_porciento))
         .map(d => d.nombre);
+      x = d3.scaleLinear()
+        .domain([0, d3.max(dataAll, d => d.total_porciento)])
+        .range(xScaleRange);
       updateBarChart(dataAll, sortedData);
       break;
     case 5:
@@ -187,6 +201,9 @@ let drawBySlide = () => {
       value_key = "total_porciento";
       var sortedData = dataG.sort((a, b) => d3.descending(a.total, b.total))
         .map(d => d.nombre);
+      x = d3.scaleLinear()
+        .domain([0, d3.max(dataAll, d => d.total_porciento)])
+        .range(xScaleRange);
       updateBarChart(dataG, sortedData);
       break;
     case 4:
@@ -197,6 +214,9 @@ let drawBySlide = () => {
       total_key = "total";
       var sortedData = dataG.sort((a, b) => d3.descending(a.total, b.total))
         .map(d => d.nombre);
+      x = d3.scaleLinear()
+        .domain([0, d3.max(dataAll, d => d.total_porciento)])
+        .range(xScaleRange);
       updateBarChart(dataG, sortedData);
       break;
 
@@ -208,6 +228,9 @@ let drawBySlide = () => {
       total_key = "total_porcentaje";
       var sortedData = dataG.sort((a, b) => d3.descending(a.total, b.total))
         .map(d => d.nombre);
+      x = d3.scaleLinear()
+        .domain([0, 100])
+        .range(xScaleRange);
       updateBarChart(dataG, sortedData);
       break;
   }
@@ -220,10 +243,12 @@ let readData = () => {
     (d, i) => {
       if (!d.codigo.includes("|")) {
         d.municipios = []
+        d.codigo = (d.codigo.length === 1 ? "0" : "") + d.codigo;
         result[d.codigo] = d;
       } else {
         let k_d = d.codigo.split("|")[0];
-        d.codigo = d.codigo.split("|")[1];
+        k_d = (k_d.length === 1 ? "0" : "") + k_d;
+        d.codigo = k_d  + d.codigo.split("|")[1];
         result[k_d].municipios.push(d);
       }
       d.hombre_casos = +d.hombre_casos;
